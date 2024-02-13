21d5755b715ec74be37b799453935feac28c67aa
Thadeu <marciothadeu1984@gmail.com>
2024-02-13 09:45:54 -0300

    feat: modified:   .github/workflows/main.yml         new file:   EKS/prod/backend.tf         new file:   EKS/prod/main.tf         new file:   EKS/prod/provider.tf         new file:   EKS/prod/terraform.tfvars         new file:   EKS/prod/variables.tf

---
+++ EKS/prod/main.tf
@@ -0,0 +1,68 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = var.region
+}
+
+module "matrix_vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.1.1"
+
+  name            = "matrix-vpc"
+  cidr            = var.vpc_cidr
+  private_subnets = var.private_subnet_cidr_blocks
+  public_subnets  = var.public_subnet_cidr_blocks
+  azs             = var.azs
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    "kubernetes.io/cluster/matrix-eks" = "shared"
+  }
+
+  public_subnet_tags = {
+    "kubernetes.io/cluster/matrix-eks" = "shared"
+    "kubernetes.io/role/elb"           = 1
+  }
+  private_subnet_tags = {
+    "kubernetes.io/cluster/matrix-eks" = "shared"
+    "kubernetes.io/role/internal-elb"  = 1
+  }
+
+}
+
+module "matrix_eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "~> 19.0"
+
+  cluster_name    = "matrix-prod"
+  cluster_version = "1.28"
+
+  subnet_ids                     = module.matrix_vpc.private_subnets
+  vpc_id                         = module.matrix_vpc.vpc_id
+  cluster_endpoint_public_access = true
+
+  eks_managed_node_groups = {
+    node-app = {
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+
+      instance_types = ["t3.medium"]
+    }
+  }
+  
+   tags = {
+    Name        = "matrix-prod"
+    Environment = "prod"
+  }
+}
